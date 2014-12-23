6550b35f8b24b305e49c0b11b9106b8504ea3f7c
mahmut.bilgen <mahmut.bilgen@gmail.com>
2014-12-24 01:15:25 +0200

    Initial checkin of the test codes.

---
+++ Test3Prj/src/TestPkg/HelloWorld.java
@@ -0,0 +1,14 @@
+package TestPkg;
+/*
+ * Test desc 
+ */
+public class HelloWorld {
+/*
+ * Same text same text
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
